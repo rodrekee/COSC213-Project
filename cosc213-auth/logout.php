97306c7b73c799eb1f2649a569051062328742af
Rodrigo Pontes Langhi Marini <joana.rodrigomarini@gmail.com>
2025-11-25 20:04:49 -0800

    Refine logout: clear session and redirect to login

--- cosc213-auth/logout.php
+++ cosc213-auth/logout.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types=1);
+
 session_start();
 
+$_SESSION = [];
 session_unset();
 session_destroy();
 
-header("Location: login.php");
+header('Location: login.php');
 exit;
